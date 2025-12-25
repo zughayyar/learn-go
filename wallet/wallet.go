42ec43c0e48c33e477462c61ec1109b54e4c5d73
Anas Zughayyar <anas.ezzughayyar@gmail.com>
2025-12-25 15:02:18 +0200

    structs and methods

---
+++ wallet/wallet.go
@@ -0,0 +1,29 @@
+package wallet
+
+import "fmt"
+
+type Bitcoin int
+type Wallet struct {
+	balance Bitcoin
+}
+
+type Stringer interface {
+	String() string
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
+	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
